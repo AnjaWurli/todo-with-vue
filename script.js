032a614e3cac7bdb8fc89905d1696daa5ea174c9
Anja Wurlitzer <anja.wurlitzer.coding@gmail.com>
2023-02-20 13:05:40 +0100

    init commit

---
+++ script.js
@@ -0,0 +1,47 @@
+Vue.createApp({
+  data() {
+    return {
+      todos: [
+        { id: 1, description: "Learn Vue", done: false },
+        { id: 2, description: "Dont get confused", done: true },
+      ],
+      newTodo: "",
+      currentFilter: "",
+      error: "",
+    };
+  },
+  computed: {
+    filterElements() {
+      return this.todos.filter((todo) => {
+        if (this.currentFilter === "done") {
+          return todo.done === true;
+        } else if (this.currentFilter === "open") {
+          return todo.done === false;
+        } else {
+          return true;
+        }
+      });
+    },
+  },
+  methods: {
+    addTodo() {
+      this.error = "";
+
+      this.todos.forEach((todo) => {
+        if (this.newTodo === todo.description) {
+          this.error = "double";
+        }
+      });
+      if (this.newTodo.length >= 5 && this.error !== "double") {
+        this.todos.push({
+          id: +new Date(),
+          description: this.newTodo,
+          done: false,
+        });
+        this.newTodo = "";
+      } else if (this.newTodo.length < 5) {
+        this.error = "tooShort";
+      }
+    },
+  },
+}).mount("#app");
